609ad5dbf71580b9746cd6b2bedf37edb346da14
AnuKumaravelan <74986860+AnuKumaravelan@users.noreply.github.com>
2021-02-28 16:37:13 -0800

    second commit by anu

--- NextDoorDocApp/app/src/main/java/com/example/nextdoordocapp/MainActivity.java
+++ NextDoorDocApp/app/src/main/java/com/example/nextdoordocapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Commit by Anu   try 2*/
+        // ANU
     }
 }
